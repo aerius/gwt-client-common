8af425681dbf414f97d0611e0c25bbe8d26ac5bf
JornC <jorncruijsen@gmail.com>
2021-08-04 12:36:26 +0200

    Added aerius platform comments to gwt-client-common Command

--- gwt-client-common/src/main/java/nl/aerius/wui/command/Command.java
+++ gwt-client-common/src/main/java/nl/aerius/wui/command/Command.java
@@ -14,11 +14,28 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/.
  */
-package nl.aerius.wui.command;
+package nl.overheid.aerius.wui.command;
 
 import com.google.web.bindery.event.shared.Event;
 
+/**
+ * A {@link Command} is a call to change the state of something.
+ * This can be a global context variable or a ui state.
+ * If the command is handled a Event will be fired to report the state was changed.
+ * Those who are responsible for the state change need to listen for commands.
+ * Those who need to react to this state change need to listen to the events.
+ *
+ * The event following the command can be silenced by setting the silence value to true.
+ *
+ * @param <H> The corresponding event class.
+ */
 public interface Command<H extends Event<?>> {
+
+  /**
+   * Sets if the event for this should be silenced or not.
+   *
+   * @param silent if true the event following this command will not be fired.
+   */
   void setSilent(boolean silent);
 
   /**
@@ -30,7 +47,13 @@ public interface Command<H extends Event<?>> {
     setSilent(true);
   }
 
+  /**
+   * @return Returns true if no event should be fired following this command.
+   */
   boolean isSilent();
 
+  /**
+   * @return Get a corresponding event instance for this command.
+   */
   H getEvent();
 }
